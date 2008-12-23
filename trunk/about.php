9f57e892d37d3d912856366cf65cb93f8c22eaaa
meixiaoqiu <meixiaoqiu@796e5ffa-c366-11dd-b16b-519e865567fa>
2008-12-23 16:50:21 +0000

    用wp和ngg插件重构foto

--- trunk/about.php
+++ trunk/about.php
@@ -4,7 +4,7 @@ require_once './inc/html/head.html';
 ?>
 <div id='main'>
 <br /><br /><br />
-<p>colorful world,young body,pure desire,true dream,crazy movement</p>
+<p>colorful world,young body,pure desire,real dream,crazy movement</p>
 <p>多彩的世界.年轻的身体.纯粹的愿望.真实的梦境.疯狂的行为</p>
 <p>die bunte Welt.der junge Körper.die reine Erwartung.der wirkliche Traum.die verrückte Tat</p><br /><br /><br />
 </div>
---
+++ trunk/inc/php/cfg.php
@@ -0,0 +1,6 @@
+<?php
+$cfg['baseurl']='http://stat001.tfbj.cc/';
+$cfg['trueurl']='http://foto.tfbj.cc/wp-content/gallery/';
+$cfg['sitename']='FOTO!';
+$cfg['sitetitle']=' | FOTO my life';
+?>
