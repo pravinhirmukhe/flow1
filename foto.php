e476545301080fecfbc8861cb8eafbcf7ab10094
meixiaoqiu <meixiaoqiu@796e5ffa-c366-11dd-b16b-519e865567fa>
2008-12-23 16:50:21 +0000

    用wp和ngg插件重构foto

--- foto.php
+++ foto.php
@@ -1,26 +1,30 @@
 <?php
-//引入图片插件入口文件
 require_once('./wp-content/plugins/nextgen-gallery/ngg-config.php');
 require_once('./inc/php/cfg.php');
-//获取最后一张图片
-$fotos=nggdb::find_last_images(0,1);
-$foto=$fotos[0];
-//获取图片tags
-$tags=wp_get_object_terms($foto->pid,'ngg_tag');
-//修改图片链接为分流网址
+
+if(!empty($_GET['pid'])){
+	$foto=nggdb::find_image($_GET['pid']);
+	$tags=wp_get_object_terms($_GET['pid'],'ngg_tag');
+}else{
+	$fotos=nggdb::get_random_images();
+	$foto=$fotos[0];
+	$tags=wp_get_object_terms($foto->pid,'ngg_tag');
+	}
 $foto_url=str_replace($cfg['trueurl'],$cfg['baseurl'],$foto->imageURL);
 //输出$title
-$title='INDEX'.$cfg['sitetitle'];
+$title=$foto->alttext.$cfg['sitetitle'];
 require_once './inc/html/head.html';
 ?>
 <div id='main'>
-  <p><?php echo $foto->imagedate; ?></p>
+	<div class="guanggao"></div>
   <p> <a href="foto.php" title="rand foto"> <img class="alignnone" title="<?php echo $foto->alttext; ?>" src="<?php echo $foto_url; ?>" alt="<?php echo $foto->alttext; ?>"> </a></p>
+  <a href="foto.php?pid=<?php echo $foto->pid - 1; ?>">« Previous</a> <a href="foto.php">Random</a> <a href="foto.php?pid=<?php echo $foto->pid + 1; ?>">Next »</a>
   <p>Tags:
   	<?php if (!empty($tags)) { foreach($tags as $tag){ ?>
 	    <a href="tags.php?tag=<?php echo $tag->term_id; ?>"><?php echo $tag->slug; ?></a>
 		<?php } } else{ echo 'none'; } ?>
   </p>
+	<div class="guanggao"></div>
 </div>
-<?php
+<?php 
 require_once './inc/html/foot.html';?>
