96b0e73024516e46a71f257f7ae0ad2e1141492a
meixiaoqiu <meixiaoqiu@796e5ffa-c366-11dd-b16b-519e865567fa>
2009-01-12 16:44:25 +0000

    修改站点description.修改foto50k的sitemap 加入权重标签.新增20090112备份数据库

--- inc/php/cfg.php
+++ inc/php/cfg.php
@@ -4,8 +4,8 @@ $cfg['siteurl']='http://foto.tfbj.cc/';
 $cfg['baseurl']='http://stat001.tfbj.cc/';
 $cfg['trueurl']='http://foto.tfbj.cc/wp-content/gallery/';
 $cfg['sitename']='FOTO';
-$cfg['sitetitle']=' | foto my life';
-$cfg['description']='To find all beautiful things';
+$cfg['sitetitle']=" | foto my life | foto your life | foto everyone's life";
+$cfg['description']='To find all beautiful things | colorful world,young body,pure desire,real dream,crazy movement';
 $cfg['keywords']='photo,graphic,shot,polaroid,camera,perfect,gorgeous,picture,nice,sex,babes';
 
 //处理图片url 隐藏真实地址
