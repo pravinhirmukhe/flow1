3c20fb1e632522bc609f12bf3f47a8016c35a6f6
meixiaoqiu <meixiaoqiu@796e5ffa-c366-11dd-b16b-519e865567fa>
2009-01-13 09:43:55 +0000

    新增判断浏览器输出moz5.0专属css属性 全体页面通过w3c双验证并加入w3c的icon

--- about.php
+++ about.php
@@ -1,5 +1,7 @@
 <?php
-$page_title='FOTO';
+
+//引入cfg文件
+require_once('./inc/php/cfg.php');
 $title='About'.$cfg['sitetitle'];
 require_once './inc/html/head.html';
 ?>
@@ -8,6 +10,7 @@ require_once './inc/html/head.html';
 <p>colorful world,young body,pure desire,real dream,crazy movement</p>
 <p>多彩的世界.年轻的身体.纯粹的愿望.真实的梦境.疯狂的行为</p>
 <p>die bunte Welt.der junge Körper.die reine Erwartung.der wirkliche Traum.die verrückte Tat</p><br /><br /><br />
+
 </div>
 <?php
 require_once './inc/html/foot.html';?>
--- foto.php
+++ foto.php
@@ -46,10 +46,10 @@ require_once './inc/html/head.html';
 ?>
 
 <div id='main'>
-  <p><img title="<?php echo $foto->alttext; ?>" src="<?php echo $foto_url; ?>" alt="<?php echo $foto->alttext; ?>"></p>
+  <p><img title="<?php echo $foto->alttext; ?>" src="<?php echo $foto_url; ?>" alt="<?php echo $foto->alttext; ?>" /></p>
   <a href="/foto/<?php echo $foto->pid - 1; ?>.html">« Previous</a> <a href="/foto.php">Random</a> <a href="/foto/<?php echo $foto->pid + 1; ?>.html">Next »</a>
   <div class="google">
-		<script type="text/javascript"><!--
+    <script type="text/javascript"><!--
 		google_ad_client = "pub-6834157029902877";
 		/* 728x90, 创建于 09-1-6 */
 		google_ad_slot = "6778785642";
@@ -57,7 +57,7 @@ require_once './inc/html/head.html';
 		google_ad_height = 90;
 		//-->
 		</script>
-		<script type="text/javascript"
+    <script type="text/javascript"
 		src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 		</script>
   </div>
