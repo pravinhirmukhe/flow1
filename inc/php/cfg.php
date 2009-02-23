954a7e769b7284a44005542ac35a103e0fe6b122
meixiaoqiu <meixiaoqiu@796e5ffa-c366-11dd-b16b-519e865567fa>
2009-02-23 14:34:55 +0000

    备份 sitemap 页面都加入canonical标签

--- inc/php/cfg.php
+++ inc/php/cfg.php
@@ -7,10 +7,28 @@ $cfg['sitename']='FOTO';
 $cfg['sitetitle']=" | foto my life | foto your life | foto everyone's life";
 $cfg['description']='To find all beautiful things | colorful world,young body,pure desire,real dream,crazy movement';
 $cfg['keywords']='photo,graphic,shot,polaroid,camera,perfect,gorgeous,picture,nice,sex,babes';
+$canonical = canonical();
 
 //处理图片url 隐藏真实地址
 function fotourl($url){
 	global $cfg;
 	return str_replace($cfg['trueurl'],$cfg['baseurl'],$url);
+}
+
+//生成canonical标签内容
+function canonical($id=0,$slug=''){
+	global $cfg;
+	switch($_SERVER['SCRIPT_NAME']){
+		case '/foto.php':
+			return $cfg['siteurl'].'foto/'.$id.'.html';
+		break;
+
+		case '/tag.php':
+			return $cfg['siteurl'].'tag/'.$id.'-'.$slug.'.html';
+		break;
+
+		default:
+			return $cfg['siteurl'].substr($_SERVER['SCRIPT_NAME'],1);
+	}
 }
 ?>
